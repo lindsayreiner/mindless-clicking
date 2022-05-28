45793d96d69c255256176ab7b9818aca4d4f9fb9
Lindsay Reiner <lindsayreiner@hey.com>
2022-05-28 16:41:59 -0400

    created react app and added beginning files from previous repo

---
+++ mindless-clicking-app/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <>
+            <footer style={{ position: 'absolute', bottom: 0 }}><span>Pass some time</span></footer>
+        </>
+    )
+}
+
