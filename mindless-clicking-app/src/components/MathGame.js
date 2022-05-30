9ffbdbca59bdd0cdecb2403e7d5182858dad30ce
Lindsay Reiner <lindsayreiner@hey.com>
2022-05-29 21:07:47 -0400

    layout of math game & timer

--- mindless-clicking-app/src/components/MathGame.js
+++ mindless-clicking-app/src/components/MathGame.js
@@ -1,10 +1,60 @@
-import React from 'react'
+import { useState } from 'react'
+
+import Timer from './Timer';
 
 export default function MathGame() {
+    const [score, setScore] = useState(0);
+    const [rn1, setRn1] = useState();
+    const [rn2, setRn2] = useState();
+    const [sign, setSign] = useState();
+
+    // const rn1 = Math.round(Math.random());
+    // const rn2 = Math.round(Math.random());
+
+    const getRandomInt = () => {
+        const minNum = 1;
+        const maxNum = 9
+
+        const rn = Math.round(Math.floor(Math.random() * (maxNum - minNum + 1) + minNum));
+
+
+        return rn;
+
+    }
+
+    const getRandomSign = () => {
+        const signs = ['+', '-', '*', '/'];
+        const randomSign = Math.round(Math.floor(Math.random() * signs.length));
+        return signs[randomSign];
+    };
+
+    const checkAnswer = (e) => {
+        e.preventDefault();
+
+        const answerCheck = eval(``)
+    }
+
     return (
         <>
-            <h1>Get it right, earn a point. Get it wrong, lose a point. Repeat.</h1>
+            <section className="mathGameWrapper">
+                <h1 className="mathGameHeader">Get it right, earn a point. Get it wrong, lose a point. Repeat.</h1>
+                <div className="scoreTimerWrapper">
+                    <p className="score">Score: {score}</p>
+                    <Timer />
+                </div>
+                <p>Calculate:</p>
+                <div className="problemWrapper">
+                    <div className="randomNumber">{getRandomInt()}</div>
+                    <div className="sign">{getRandomSign()}</div>
+                    <div className="randomNumber">{getRandomInt()}</div>
+                    <span className="sign">=</span>
+                </div>
+                <input type="number" className="answerInput" onSubmit={checkAnswer} />
 
+
+
+
+            </section>
         </>
     )
 }
